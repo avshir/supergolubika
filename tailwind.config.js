88b0ad644ca7040612aca84927172670c7f6f494
Anna Shirinskaya <av.shirinskaya@gmail.com>
2024-11-28 21:36:52 +0100

    feat: update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,64 @@
+const colors = require("tailwindcss/colors");
+
+//use: 
+//indigo
+//blue
+//violet
+//green
+//gray
+//orange
+//red
+//neutral
+//white, black
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  // darkMode: "selector",
   theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+    colors: {
+      primary: {
+        DEFAULT: colors.blue[950],
+        // DEFAULT: "#172554",
+      },
+      secondary: { ...colors.blue, DEFAULT: colors.blue[300] },
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.neutral,
+      neutral: colors.neutral,
+      blue: colors.blue,
 
+      purple: colors.purple,
+      violet: colors.violet,
+      indigo: colors.indigo,
+      green: colors.green,
+      rose: colors.rose,
+      pink: colors.pink,
+      fuchsia: colors.fuchsia,
+      sky: colors.sky,
+      cyan: colors.cyan,
+      teal: colors.teal,
+      emerald: colors.emerald,
+      lime: colors.lime,
+      yellow: colors.yellow,
+      amber: colors.amber,
+      orange: colors.orange,
+      red: colors.red,
+      slate: colors.slate,
+      zinc: colors.zinc,
+      stone: colors.stone,
+    },
+    extend: {
+      colors: {
+        accent: {
+          DEFAULT: colors.blue[300],
+          green: "#A3D29D",
+          beige: "#E5CAB3",
+          lavander: colors.blue[300],
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
