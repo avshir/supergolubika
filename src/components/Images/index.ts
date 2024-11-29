cd30b54285687769197f64ce41696b7bbdf0e518
Anna Shirinskaya <av.shirinskaya@gmail.com>
2024-11-29 12:04:20 +0100

    feat: update assets

--- src/components/Images/index.ts
+++ src/components/Images/index.ts
@@ -1,7 +1,7 @@
-import bg_main_sreen_golubika_IMG from "./../../assets/images/joanna-kosinska-4qujjbj3srs-unsplash_1920-1282.jpg";
-import golubika_kids_hand_IMG from "./../../assets/images/davies-designs-studio-k4SiuG9YzdE-unsplash_640.jpg";
-import golubika4_1_PNG from "./../../assets/images/png/golubika-1-without_bg.png";
-import golubika4_2_PNG from "./../../assets/images/png/golubika-2-without_bg.png";
+import bg_main_sreen_golubika_IMG from "./../../assets/images/golubika_food_main_screen_1920.jpg";
+import golubika_kids_hand_IMG from "./../../assets/images/golubika_food_kids_640.jpg";
+import golubika4_1_PNG from "./../../assets/images/png/golubika_1_256.png";
+import golubika4_2_PNG from "./../../assets/images/png/golubika_2_256.png";
 
 export {
   bg_main_sreen_golubika_IMG,
@@ -11,5 +11,7 @@ export {
 };
 
 // images as bg css
-export const bgImg_blueberry_many_1 = "bg-[url('./assets/images/1000_F_120890045_V1xVnuyS1uI1FG41KjQLPYPSakHCIvYd.jpg')]"
-export const bgImg_blueberry_food_1 = "bg-[url('./assets/images/joanna-kosinska-4qujjbj3srs-unsplash_1920-1282.jpg')]"
+export const bgImg_blueberry_many_1 =
+  "bg-[url('./assets/images/golubika_many_berries_960.jpg')]";
+export const bgImg_blueberry_food_1 =
+  "bg-[url('./assets/images/golubika_food_main_screen_1920.jpg')]";
