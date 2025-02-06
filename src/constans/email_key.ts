1bce8e8f53736b56348a3c3cab1cee528b43a7ec
Anna Shirinskaya <av.shirinskaya@gmail.com>
2025-02-06 14:06:44 +0100

    fix: hide EMAIL_ACCESS_KEY using .env.local variable

--- src/constans/email_key.ts
+++ src/constans/email_key.ts
@@ -1,4 +1,4 @@
-// web3forms.com for Contact Form
+// web3forms.com/platforms/react-contact-form for Contact Form
 // Public Access Key is:
 // for email info@supergolubika.by:
-export const EMAIL_ACCESS_KEY = "0f11f423-62c4-4f38-9fd5-3bc5cfd1fe00";
+export const EMAIL_ACCESS_KEY = import.meta.env.VITE_EMAIL_ACCESS_KEY;
